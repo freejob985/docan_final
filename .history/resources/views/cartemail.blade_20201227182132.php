715681e8170eeb49ee75836aca7657869026bb66
freejob985 <freejob985@gmail.com>
2020-12-27 18:21:48 +0200

    ss

--- .history/resources/views/cartemail.blade_20201227182132.php
+++ .history/resources/views/cartemail.blade_20201227182132.php
@@ -2,7 +2,7 @@
 @php
 	echo $array['content'];
 @endphp
-<a href="<?php echo  $array['url'] " ?> >Please click on the link to continue shopping</a>
+<a href="<?php echo  $array['url'] " >Please click on the link to continue shopping</a>
 Thanks,<br>
 {{ config('app.name') }}
 @dd("Catch errors for script and full tracking ( 1)");
