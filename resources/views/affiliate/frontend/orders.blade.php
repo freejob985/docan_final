1e7079d7b35628be41f6b8809b02325c9b205283
freejob985 <freejob985@gmail.com>
2021-01-06 14:38:03 +0200

    asd

--- resources/views/affiliate/frontend/orders.blade.php
+++ resources/views/affiliate/frontend/orders.blade.php
@@ -184,7 +184,161 @@ table.dataTable tbody th, table.dataTable tbody td {
                     <div class="card no-border mt-5">
                         <a class="btn btn-info" href="{{url('data-pdf')}}">Export To PDF</a>
                         <div class="card-body cc">
-
+                            <table class="example table table-responsive table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>رقم الوصفه</th>
+                                        <th> اسم العميل</th>
+                                        <th>اللينك</th>
+                                        <th>الوصفه الطبيه</th>
+                                        <th>التاريخ</th>
+                                        <th>السعر الكلي</th>
+                                        <th> العموله</th>
+                                        <th>عرض</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($orders as $order)
+                                    <tr>
+                                        <th
+                                            style="font-size: 398%;vertical-align: revert;text-align: center;background: #17a2b8;color: white;">
+                                            {{ $order->id}}</th>
+                                        <th>
+                                            @php $reals = App\Order::where('token', '=',$order->token)->latest()->get();
+                                            $id_css++; @endphp
+                                            @foreach ($reals as $real)
+                                            <ul class="main-ul">
+                                                @php $delivery_status = $real->orderDetails->first()->delivery_status;
+                                                @endphp
+                                                <li style="display: grid;"> {{ $real->user->name }} </li>
+                                                <li style="display: grid;"> {{ $real->user->email }} </li>
+                                                <li style="
+                                                display: grid;
+                                            "> {{ $real->user->phone }} </li>
+                                                <li> @if ($delivery_status == 'pending') <span
+                                                        class="label label-danger">
+                                                        {{translate('Pending')}} </span>
+                                                    @endif
+                                                    @if ($delivery_status == 'on_review') <span
+                                                        class="label label-warning"> {{translate('On review')}} </span>
+                                                    @endif
+                                                    @if ($delivery_status == 'on_delivery') <span
+                                                        class="label label-info"> {{translate('On delivery')}} </span>
+                                                    @endif
+                                                    @if ($delivery_status == 'delivered') <span
+                                                        class=" label label-success"> {{translate('Delivered')}} </span>
+                                                    @endif
+                                                </li>
+                                                <hr>
+                                                <table class="table table-bordered">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>#</th>
+                                                            <th>{{ translate('Name') }}</th>
+                                                            <th>{{translate('Quantity')}}</th>
+                                                            <th>{{translate('Price')}}</th>
+                                                            <th>{{translate('Tax')}}</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        @foreach ($real->orderDetails as $index=>$product)
+                                                        <tr>
+                                                            <th>{{ $index + 1  }}</th>
+                                                            <th>
+                                                                {{ lang($product->product->id,Session::get('locale')) }}
+                                                            </th>
+                                                            <th>{{ $product->quantity }}</th>
+                                                            <th>{{ $product->price }}</th>
+                                                            <th>{{ $product->tax }}</th>
+                                                        </tr>
+                                                        @endforeach
+                                                    </tbody>
+                                                </table>
+                                                <div class="container">
+                                                </div>
+                        </div>
+                        </ul>
+                        @endforeach
+                        <hr>
+                        </th>
+                        <th>{{ route('getUserSendCart', $order->token) }}</th>
+                        <th>
+                            <table class="sss">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>{{ translate('Name') }}</th>
+                                        <th>{{translate('Quantity')}}</th>
+                                        <th>{{translate('Price')}}</th>
+                                        <th>{{translate('Tax')}}</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @php
+                                    $products = \App\Sendcartuser::where('order_id', '=',
+                                    $order->id)->get();
+                                    @endphp
+                                    <ul class="list-group">
+                                        @if (count($products)>0)
+                                        @foreach (\App\Sendcartuser::where('order_id', '=',
+                                        $order->id)->take(1)->get() as $item_countries)
+                                        <li class="list-group-item">الاسم :{{$item_countries->name}}</li>
+                                        <li class="list-group-item"> الموبيل:{{$item_countries->pas}}</li>
+                                        <li class="list-group-item"> البريد الالكتروني:{{$item_countries->email}}</li>
+                                        @endforeach
+                                        @endif
+                                        <li class="list-group-item">
+                                <tbody>
+                                    @foreach ($products as $index=>$product)
+                                    <tr>
+                                        <th>{{ $index + 1  }}</th>
+                                        <th>
+                                            {{ lang($product->product->id,Session::get('locale')) }}
+                                        </th>
+                                        <th>{{ $product->quantity }}</th>
+                                        <th>{{ $product->price }}</th>
+                                        <th>{{ $product->tax }}</th>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                                </li>
+                                </ul>
+                                <tr>
+                                </tr>
+                                </tbody>
+                            </table>
+                            <div id="demo{{ $id_css   }}" class="collapse">
+                            </div>
+                        </th>
+                        <th tyle="
+    vertical-align: revert;
+    text-align: center;
+">{{  $order->created_at }}</th>
+                        <th tyle="
+    vertical-align: revert;
+    text-align: center;
+">{{ $order->total }}</th>
+                        <th tyle="
+    vertical-align: revert;
+    text-align: center;
+">
+                            @php
+                            $option = App\AffiliateOption::where('type', '=',
+                            'product_sharing')->first();
+                            $comisstion = $option->percentage;
+                            $balance = $order->total / $comisstion;
+                            @endphp
+                            {{  $balance }}
+                        </th>
+                        <th style="
+    vertical-align: revert;
+    text-align: center;
+"> <a href="{{ route('affiliate.getsingleorder', $order->id) }}"> <span class="btn btn-info">
+                                    <i class="fa fa-eye"></i> </span> </a></th>
+                        </tr>
+                        @endforeach
+                        </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
